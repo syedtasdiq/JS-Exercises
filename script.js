c9627e9541846c0eacfaf42bb6b1836738b5ed9d
cis101 <cis101@secs.iub.edu.bd>
2019-09-30 09:34:54 +0600

    function adder

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ console.log("External JS")
 
 function arraySum (arr){
     let sum =0
-    arr.forEach(element => {
-        sum = sum + element
+   //arr.forEach(element => {
+     //   sum = sum + element
 
+//})
+arr.forEach(function(element){
+    sum = sum + element
 })
-
-return sum
+return  (multiplier) =>{
+    return sum*multiplier
+}
 
 }
 
 let sum = arraySum ([1,2,3])
-console.log (sum)
+console.log (sum(2))
